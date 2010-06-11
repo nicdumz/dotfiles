65889af7652de747990138a5eb242a8165d9fb42
Nicolas Dumazet <nicdumz.commits@gmail.com>
2010-06-11 19:26:20 +0900

    switching to rxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -37,7 +37,7 @@ theme_path = "/home/nicdumz/.config/awesome/themes/default/theme.lua"
 beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
