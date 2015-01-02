a1f537ee13bb4700125824ef519970cddaab6b94
Nicolas Dumazet <nicdumz.commits@gmail.com>
2015-01-02 11:26:39 +0100

    Small installation script.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Nicolas' dotfiles #
 
 Versioning together my dotfiles. Some of them may be useful to you, but I doubt it :)
+
+# Installation
+
+    python setup.py
---
+++ setup.py
@@ -0,0 +1,28 @@
+# Helper to setup small redirects to versioned copies of config:
+# Assuming a set of versioned skel.foo files, this will create copy skel.foo to
+# ~/.foo
+#
+# Most skeleton files will only 'import' a versioned copy from the correct
+# directory.
+import os
+import shutil
+
+files = ['.gitconfig', '.hgrc', '.vimrc']
+
+destination = os.path.expanduser('~')
+source = os.path.dirname(os.path.realpath(__file__))
+
+for f in files:
+    full = os.path.join(destination, f)
+    if os.path.exists(full):
+        raise RuntimeError("%s already exists, can't create skeleton there." % full)
+
+for f in files:
+    full = os.path.join(source, 'skel' + f)
+    assert os.path.exists(full), "Skeleton %s should exist" % full
+
+for f in files:
+    full_source = os.path.join(source, 'skel' + f)
+    full_destination = os.path.join(destination, f)
+    shutil.copyfile(full_source, full_destination)
+    shutil.copymode(full_source, full_destination)
