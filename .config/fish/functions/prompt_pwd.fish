a1c5089c3c2862c4690b7bd360ecddaf8a7cfaca
Nicolas Dumazet <nicdumz.commits@gmail.com>
2015-09-14 16:14:39 +0200

    Allow for prompt customization.

--- .config/fish/functions/prompt_pwd.fish
+++ .config/fish/functions/prompt_pwd.fish
@@ -1,3 +1,9 @@
 function prompt_pwd --description 'Print the current working directory, shortend to fit the prompt'
-	echo $PWD  | sed -e "s|^$HOME|~|"
+    set p (echo $PWD | sed -e "s|^$HOME|~|")
+    # Provide customization possibilities for local machines.
+    if functions -q prompt_pwd_after
+        echo $p | prompt_pwd_after
+    else
+        echo $p
+    end
 end
