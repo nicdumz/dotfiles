e645f22508ca6fab989798c4929747c215b48b82
Nicolas Dumazet <nicdumz.commits@gmail.com>
2017-08-11 15:08:53 +0200

    brew setup update

--- install_brew.sh
+++ install_brew.sh
@@ -9,7 +9,7 @@ if [ -d "$HOME/Library" ]; then
     else
         git clone https://github.com/Homebrew/brew.git ~/brew
     fi
-    cat ~/.dotfiles/brew_recipes.txt | xargs -n 1 ~/brew/bin/brew install
+    cat ~/.dotfiles/brew_leaves.txt | xargs -n 1 ~/brew/bin/brew install
     cat ~/.dotfiles/brew_taps.txt | xargs -n 1 ~/brew/bin/brew tap
     cat ~/.dotfiles/brew_casks.txt | xargs -n 1 ~/brew/bin/brew cask install
     ~/brew/bin/brew update
