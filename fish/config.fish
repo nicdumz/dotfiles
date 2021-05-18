d2657f72872308bae3060382b8bda3687ee285bd
Nicolas Dumazet <ndumazet@google.com>
2021-05-18 09:26:46 +0200

    Actually version tide vars

--- fish/config.fish
+++ fish/config.fish
@@ -51,6 +51,8 @@ end
 
 set fish_color_command green
 set fish_color_autosuggestion white
+
+# Tide setup
 set tide_context_always_display true
 set tide_context_bg_color green
 set tide_context_default_color black
@@ -61,3 +63,63 @@ set tide_pwd_bg_color blue
 set tide_status_failure_bg_color red
 set tide_status_failure_color yellow
 set tide_status_success_color green
+set tide_always_display true
+set tide_cmd_duration_bg_color C4A000
+set tide_cmd_duration_color 000000
+set tide_cmd_duration_icon 
+set tide_git_bg_color 4E9A06
+set tide_git_branch_color 000000
+set tide_git_conflicted_color 000000
+set tide_git_dirty_color 000000
+set tide_git_operation_color 000000
+set tide_git_staged_color 000000
+set tide_git_stash_color 000000
+set tide_git_untracked_color 000000
+set tide_git_upstream_color 000000
+set tide_jobs_bg_color 444444
+set tide_jobs_color 4E9A06
+set tide_left_prompt_frame_color 585858
+set tide_left_prompt_frame_enabled true
+set tide_left_prompt_item_separator_diff_color 
+set tide_left_prompt_item_separator_same_color 
+set tide_left_prompt_pad_items true
+set tide_left_prompt_prefix 
+set tide_left_prompt_suffix 
+set tide_nvm_bg_color 75507B
+set tide_nvm_color 2E3436
+set tide_os_bg_color CED7CF
+set tide_os_color 080808
+set tide_print_newline_before_prompt false
+set tide_prompt_connection_color 585858
+set tide_pwd_anchors 'first'  'last'  'google3'
+set tide_pwd_color_anchors E4E4E4
+set tide_pwd_color_dirs E4E4E4
+set tide_pwd_color_truncated_dirs BCBCBC
+set tide_pwd_dir_icon 
+set tide_pwd_home_icon 
+set tide_right_prompt_frame_color 585858
+set tide_right_prompt_frame_enabled true
+set tide_right_prompt_item_separator_diff_color 
+set tide_right_prompt_item_separator_same_color 
+set tide_right_prompt_items 'status'  'cmd_duration'  'context'  'jobs'  'vi_mode'
+set tide_right_prompt_pad_items true
+set tide_right_prompt_prefix 
+set tide_right_prompt_suffix 
+set tide_rust_bg_color FF8700
+set tide_rust_color 2E3436
+set tide_status_always_display true
+set tide_status_success_bg_color 2E3436
+set tide_time_bg_color D3D7CF
+set tide_time_color 000000
+set tide_time_format
+set tide_vi_mode_default_bg_color 008000
+set tide_vi_mode_default_color 000000
+set tide_vi_mode_default_icon DEFAULT
+set tide_vi_mode_insert_bg_color 444444
+set tide_vi_mode_replace_bg_color 808000
+set tide_vi_mode_replace_color 000000
+set tide_vi_mode_replace_icon REPLACE
+set tide_vi_mode_visual_bg_color 000080
+set tide_vi_mode_visual_color 000000
+set tide_vi_mode_visual_icon VISUAL
+set tide_virtual_env_bg_color 444444
